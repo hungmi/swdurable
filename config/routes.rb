02cfc0e19ddf35343a0511f1498b048c9f6d7287
hungmi <hungmihungmi@gmail.com>
2014-12-13 10:07:18 +0800

    all index view edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :stocks, :processings, :offices, :homes
+  resources :stocks, :processings, :offices, :homes, :orders
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
